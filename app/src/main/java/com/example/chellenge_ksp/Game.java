e4add2375c38bb5af9b1c4046ed8614c95d493de
Andrey <32015678+Andrey017@users.noreply.github.com>
2019-11-30 20:15:26 +0300

    Add list of games and creation dialog for game

--- app/src/main/java/com/example/chellenge_ksp/Game.java
+++ app/src/main/java/com/example/chellenge_ksp/Game.java
@@ -1,9 +1,13 @@
 package com.example.chellenge_ksp;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.chellenge_ksp.RecyclerViewGame.GameAdapter;
 import com.example.chellenge_ksp.RecyclerViewGame.ItemGame;
@@ -17,16 +21,49 @@ public class Game extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private GameAdapter gameAdapter;
 
+    private Button add_new_game;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
+
+        add_new_game = (Button)findViewById(R.id.new_game_add);
+        add_new_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
+                final View view = getLayoutInflater().inflate(R.layout.create_game, null);
+                EditText editText = (EditText) view.findViewById(R.id.textView_name_game);
+                Button cancel = (Button) view.findViewById(R.id.button_cancel_game);
+                Button create = (Button) view.findViewById(R.id.button_create);
+
+                builder.setView(view);
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                create.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //тут отправка на сервер будет
+                    }
+                });
+            }
+        });
 
         buildRecyclerViewGame();
     }
 
     private void createListGame() {
         itemGameArrayList = new ArrayList<>();
-        itemGameArrayList.add(new ItemGame("dsnd", R.drawable.back_1));
+        itemGameArrayList.add(new ItemGame("Play №1", R.drawable.back_1));
+        itemGameArrayList.add(new ItemGame("Play №2", R.drawable.back_1));
     }
 
     private void buildRecyclerViewGame(){
